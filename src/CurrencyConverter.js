391af5e6d1ad6317d56b70f1e32f1d19e097aca7
lurnuku <lurnuku@yandex.com>
2024-03-04 16:00:34 +0100

    api_key

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import logo from "./pics/logo.png";
 import star from "./pics/star.png";
 
+const api_key = process.env.REACT_APP_API_KEY;
 const url =
   "https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest?from=USD&to=EUR%2CGBP";
 
@@ -11,7 +12,7 @@ const CurrencyConverter = () => {
   useEffect(() => {
     fetch(url, {
       headers: {
-        "X-RapidAPI-Key": "c6d0ac48dcmsh51514b0fadfde41p1c48fdjsned190d7ea281",
+        "X-RapidAPI-Key": api_key,
         "X-RapidAPI-Host":
           "currency-conversion-and-exchange-rates.p.rapidapi.com",
       },
